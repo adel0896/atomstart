085286236332447e1f714a2a4fc321e97c16336b
Adelina <adel0896@stud.kea.dk>
2022-01-31 13:36:39 +0100

    see if commits work

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById()
+element.addEventListener('click', function(e) {
+console.log();
+ })
+ document.addEventListener('click', function(e) {  })
+ document.addEventListener('click', function(e) {
+
+   })
+   document.addEventListener('click', function(e) {
+
+     })
+     (function() {
+       'use strict';
+       var Person = (function)
+     }());
+     document.addEventListener('', function(e) {
+
+       })
+       
